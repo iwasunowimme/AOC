5bb36c45545713deb40a90e584afe3b0735f5743
Joshua Wright <Josh.wright92@hotmail.com>
2020-12-14 17:08:54 +1100

    Updated and cleaned 2020 DAY14

--- 2020/DAY14/DAY14.py
+++ 2020/DAY14/DAY14.py
@@ -1,74 +1,89 @@
-
+'''https://adventofcode.com/2020/day/14'''
 
 import re
-from itertools import combinations
-from itertools import permutations
-from collections import defaultdict
-from collections import deque
-from copy import deepcopy
 
-
+# Input from text file
 with open("input.txt", "r") as f:
     values = [lines[:-1] for lines in f.readlines()]
 #values = ['mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X','mem[8] = 11','mem[7] = 101','mem[8] = 0']
 #values = ['mask = 000000000000000000000000000000X1001X','mem[42] = 100','mask = 00000000000000000000000000000000X0XX','mem[26] = 1']
+
+# Used to get the two numbers
 reg = re.compile(r'(\d+)')
-mem = defaultdict(int)
-mask = ''
-for line in values:
-    if line.startswith('mask'):
-        mask = line.split()[2]
-    else:
-        m = reg.findall(line)
-        mem_slot = m[0]
-        towrite = str(bin(int(m[1]))[2:].zfill(36))
-        new_string =''
-        for i in range(len(towrite)):
-            if mask[i] != 'X':
-                new_string += mask[i]
+# Use dictionary to store values to keep list small
+def solve(values, part_one):
+    mem = {}
+    mask = ''
+    # For each input
+    for line in values:
+        if line.startswith('mask'):  # Is a Mask
+            # Get mask value as string
+            mask = line.split()[2]
+
+        else:  # Is not a mask
+            address = []
+            m = reg.findall(line)  # Find all numbers
+            # First number is the memory slot
+            mem_slot = str(bin(int(m[0]))[2:].zfill(36))
+            # Second number is the string to write, convert to 36 bit binary number, padded with 0's sans '0b'
+            towrite = str(bin(int(m[1]))[2:].zfill(36))
+            new_string = ''
+
+            # If part one
+            if part_one:
+                # For each value in to write memory and mask as pairs
+                # We can use pairs since we are comparing them bitwise
+                for (writing, masking) in zip(towrite, mask):
+                    # If bit is overwritten by mask
+                    if masking != 'X':
+                        new_string += masking
+                    # If bit is not masked
+                    else:
+                        new_string += writing
+                # Convert back to decimal
+                mem[mem_slot] = int(new_string, 2)
+
+            # If part Two
             else:
-                new_string += towrite[i]
+                # Iterate through the mask and memory bit
+                for (memory_bit, masking) in zip(mem_slot, mask):
+                    # Create empty list to add memory addresses
+                    new_add = []
+                    # If masking is '1', bit is overwritten as 1
+                    if masking == '1':
+                        if address:
+                            for add in address:  # For all current addresses up to this bit
+                                new_add.append(add + '1')
+                        else:  # If empty
+                            new_add = ['1']
+                    # If masking is '0', bit is written as the bit in the given address
+                    elif masking == '0':
+                        if address:
+                            for add in address:  # For all current addresses up to this bit
+                                new_add.append(add + memory_bit)
+                        else:  # If empty
+                            new_add = [memory_bit]
+                    # If masking is 'X', bit becomes schrodinger's bit, both 0 and 1 at the same time.
+                    # This adds all combinations of the address with this bit being 0 and 1 to the list
+                    elif masking == 'X':
+                        if address:
+                            for add in address:  # For all current addresses up to this bit
+                                new_add.append(add + '1')
+                                new_add.append(add + '0')
+                        else:  # If empty
+                            new_add = ['1', '0']
 
-        mem[mem_slot] = int(new_string,2)
-print("Part1: ", sum(mem.values()))
-print(mem)
-mem2 = defaultdict(int)
-for line in values:
-    if line.startswith('mask'):
-        mask = line.split()[2]
-    else:
-        address = []
-        m = reg.findall(line)
-        mem_slot = str(bin(int(m[0]))[2:].zfill(36))
-        towrite = str(bin(int(m[1]))[2:].zfill(36))
-        new_string =''
-        for i in range(len(mask)):
-            new_add = []
-            if mask[i] == '1':
-                if address == []:
-                    new_add = ['1']
-                else:
-                    for add in address:
-                        new_add.append(add +'1')
-            elif mask[i] == '0':
-                if address == []:
-                    new_add = [mem_slot[i]]
-                else:
-                    for add in address:
-                        new_add.append(add + mem_slot[i])
-            elif mask[i] == 'X':
-                if address == []:
-                    new_add = ['1','0']
-                else:
-                    for add in address:
-                        new_add.append(add+'1')
-                        new_add.append(add+'0')
-            address = new_add
+                    # Address list is reinstated as the working ones
+                    address = new_add
 
+                # For all addresses write the memory value
+                for add in address:
+                    mem[add] = int(towrite, 2)
+    # We want the some of all address values, so we return this
+    return sum(mem.values())
 
 
-        for add in address:
-            mem2[add] = int(towrite, 2)
+print("Part1: ", solve(values, True))
+print("Part2: ", solve(values, False))
 
-print("Part2: ", sum(mem2.values()))
 
