d88e77d94b0e32298fe331323411b138bf99aa88
Joshua Wright <Josh.wright92@hotmail.com>
2021-01-29 17:51:28 +1100

    Changed use of reserved words

--- 2016/DAY06/DAY06.py
+++ 2016/DAY06/DAY06.py
@@ -3,13 +3,13 @@ from collections import Counter
 
 # Import from text file
 with open("input.txt", "r") as f:
-    input = [lines[:-1] for lines in f.readlines()]
+    values = [lines[:-1] for lines in f.readlines()]
 
 # find the most common of each column
 # Zip(*) transposes the array
 # Join makes it a string
-print("Part1:", ''.join(Counter(''.join(x)).most_common()[0][0] for x in zip(*input)))
-print("Part2:", ''.join(Counter(''.join(x)).most_common()[-1][0] for x in zip(*input)))
+print("Part1:", ''.join(Counter(''.join(x)).most_common()[0][0] for x in zip(*values)))
+print("Part2:", ''.join(Counter(''.join(x)).most_common()[-1][0] for x in zip(*values)))
 
 
 
